834dddc887b4c815b4f9f1d94fdd4b799b19299d
Karina Chernobai <karina.chernobai@gmail.com>
2025-03-05 23:56:53 +0100

    Events demo

---
+++ ConsoleApppLearn/EventsDemo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApppLearn;
+// An event in C# is a mechanism that allows a class (publisher) to notify other classes (subscribers) when something important happens.
+// Events are based on delegates and follow the Observer design pattern.
+public class EventsDemo
+{
+    public static void Demonstrate()
+    {
+        Alarm alarm = new Alarm();
+
+        // Step 4: Subscribe your even handler to the event
+        alarm.OnAlarm += AlarmTriggered;
+
+        alarm.TriggerAlarm();  // Step 5: Raise the event
+    }
+
+    static void AlarmTriggered(string message)
+    {
+        Console.WriteLine($"Event received: {message}");
+    }
+}
+
+public class Alarm
+{
+    // Step 1: Define a delegate
+    public delegate void AlarmEventHandler(string message);
+    // Step 2: Define an event
+    // event event-delegate event-name;
+    // Delegates and events are related because an event is built upon a delegate. 
+    public event AlarmEventHandler OnAlarm;
+
+    public void TriggerAlarm()
+    {
+        if (OnAlarm != null)  // Step 3: Check if event has subscribers
+        {
+            OnAlarm("Warning! The alarm is triggered.");
+        }
+    }
+}
---
+++ ConsoleApppLearn/Person.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApppLearn;
+
+class Profile
+{ }
+
+
+class Person : IEquatable<Person>, IComparable<Person>
+{
+    public bool Equals(Person other)
+    {
+        return FirstName == other.FirstName && LastName == other.LastName;
+    }
+
+    public string PassNo { get; private set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public override int GetHashCode()
+    {
+        return FirstName.GetHashCode() ^ LastName.GetHashCode();
+    }
+
+    void Demo()
+    {
+        var map = new Dictionary<Person, Profile>();
+        var mapPassNo = new Dictionary<Person, Profile>(new PassNoEqComparer());
+
+        var sortedMapByPassNo = new SortedDictionary<Person, Profile>(new PassNoComparer());
+
+        try
+        {
+
+        }
+        catch(Exception ex)
+        {
+            throw; // shows original place of Exception
+            throw ex; // rewrites Stack trace property to point to this line
+        }
+    }
+
+    public int CompareTo(Person? other)
+    {
+        throw new NotImplementedException();
+    }
+
+    class PassNoEqComparer : IEqualityComparer<Person>
+    {
+        public bool Equals(Person? x, Person? y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetHashCode([DisallowNull] Person obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class PassNoComparer : IComparer<Person>
+    {
+        public int Compare(Person? x, Person? y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
